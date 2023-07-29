58664b3e43e48a54eb3aa60debb82f6b4b9fcf91
Chris Brandt <cmbrandt199@gmail.com>
2023-07-28 23:16:02 -0400

    Adding new example files

---
+++ src/1-class-design/1-essentials/essentials_rational.cxx
@@ -0,0 +1,155 @@
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <string_view>
+
+class Rational {
+public:
+  Rational() = default;
+  Rational(int n)        : num{n}, den {1} { normalize(); }
+  Rational(int n, int d) : num{n}, den {d} { normalize(); }
+
+  // Copy constructor
+  Rational(Rational const& other) : num{other.num}, den{other.den} { }
+
+  // Copy assignment operator
+  Rational& operator=(Rational const& other)
+  {
+    num = other.num;
+    den = other.den;
+    return *this;
+  }
+
+  // Move constructor
+  Rational(Rational&& other)
+  : num{std::move(other.num)}, den{std::move(other.den)} { }
+
+  // Move assignment operator
+  Rational& operator=(Rational&& other)
+  {
+    num = std::move(other.num);
+    den = std::move(other.den);
+    return *this;
+  }
+
+  int get_num() const { return num; }
+  int get_den() const { return den; }
+
+  void set_num(int n) { num = n; }
+  void set_den(int d) { den = d; }
+
+private:
+  int gcf() const;
+  void normalize();
+
+  int num{0};
+  int den{1};
+};
+
+int Rational::gcf() const {
+  int n = std::abs(num);
+  int d = den;
+  int temp;
+
+  while (d != 0) {
+    temp = n % d;
+    n = d;
+    d = temp;
+  }
+
+  return n;
+}
+
+void Rational::normalize() {
+  // Denominator cannot equal zero
+  assert(den != 0);
+  // Unique representation for zero
+  if (num == 0)
+    den = 1;
+  // Only the numerator should be negative
+  else if (den < 0) {
+    num = -num;
+    den = -den;
+  }
+  // Reduce
+  int n = Rational::gcf();
+  num = num / n;
+  den = den / n;
+}
+
+bool operator==(Rational const& r1, Rational const& r2) {
+  int n1 = r1.get_num();
+  int d1 = r1.get_den();
+  int n2 = r2.get_num();
+  int d2 = r2.get_den();
+
+  return (n1*d2) == (d1*n2);
+}
+
+bool operator!=(Rational const& r1, Rational const& r2) {
+  return !operator==(r1, r2);
+}
+
+bool operator<(Rational const& r1, Rational const& r2) {
+  int    n1 = r1.get_num();
+  int    n2 = r2.get_num();
+  double d1 = r1.get_den();
+  double d2 = r2.get_den();
+
+  return (n1/d2) < (n2/d1);
+}
+
+bool operator>(Rational const& r1, Rational const& r2) {
+  return operator<(r2, r1);
+}
+
+bool operator<=(Rational const& r1, Rational const& r2) {
+  return !operator>(r1, r2);
+}
+
+bool operator>=(Rational const& r1, Rational const& r2) {
+  return !operator<(r1, r2);
+}
+
+void print_rational(std::string_view sv, Rational const& r) {
+  std::cout << sv
+            << "\nr.num = " << r.get_num()
+            << "\nr.den = " << r.get_den() << std::endl;
+}
+
+int main()
+{
+  Rational r1;
+  print_rational("\nDefault constructor", r1);              // = 0/1
+
+  Rational r2{5};
+  print_rational("\nInteger conversation constructor", r2); // = 5/1
+
+  Rational r3{2, 4};
+  print_rational("\nParameterized constructor", r3);        // = 1/2
+
+  Rational r4{r3};
+  print_rational("\nCopy constructor", r4);                 // = 1/2
+
+  Rational r5 = r3;
+  print_rational("\nCopy assignment operator", r5);         // = 1/2
+
+  Rational r6{std::move(r3)};
+  print_rational("\nMove constructor", r6);                 // = 1/2
+
+  Rational r7 = std::move(r4);
+  print_rational("\nMove assignment operator", r7);         // = 1/2
+
+
+  Rational c1{-1, 2};
+  Rational c2{2, -4};
+
+  std::cout << std::boolalpha
+            << "\n(-1/2) == (2/-4) = " << (c1 == c2) // = true
+            << "\n(-1/2) != (2/-4) = " << (c1 != c2) // = false
+            << "\n(-1/2) <  (2/-4) = " << (c1 <  c2) // = false
+            << "\n(-1/2) >  (2/-4) = " << (c1 >  c2) // = false
+            << "\n(-1/2) <= (2/-4) = " << (c1 <= c2) // = true
+            << "\n(-1/2) >= (2/-4) = " << (c1 >= c2) // = true
+            << std::endl;
+}
---
+++ src/1-class-design/2-special-member-functions/6_shared_ptr.cxx
@@ -0,0 +1,118 @@
+// https://godbolt.org/z/hc1sYTMGx
+
+// VERSION #6
+// STD::SHARED_PTR
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <utility>
+
+struct Resource {
+  int x{5};
+  int y{7};
+};
+
+struct Widget {
+public:
+  // Default consructor
+  Widget() = default;
+
+  // Parameterized constructor
+  Widget(int i, std::string s, Resource p)
+    : idx{i}, str{s}, ptr{std::make_shared<Resource>(p)} { }
+
+  // Copy constructor
+  Widget(Widget const& other) = default;
+
+  // Copy assignment operator
+  Widget& operator=(Widget const& other) = default;
+
+  // Move constructor
+  Widget(Widget&& other) = default;
+
+  // Move assignment operator
+  Widget& operator=(Widget&& other) = default;
+
+  // Destructor
+  ~Widget() = default;
+
+  int         get_idx() const { return idx; }
+  std::string get_str() const { return str; }
+  Resource*   get_ptr() const { return ptr.get(); }
+
+private:
+  int idx{};
+  std::string str{};
+  std::shared_ptr<Resource> ptr{};
+};
+
+// Formatted print function for Widget
+void print_widget(std::string_view s, Widget const& w)
+{
+  // Display information about the Widget
+  std::cout << s
+            << "\nidx = " << w.get_idx()
+            << "\nstr = " << w.get_str()
+            << "\nptr = " << w.get_ptr();
+
+  // If the resource is valid, get and display information about it
+  if (w.get_ptr()) {
+    auto* r = w.get_ptr();
+    std::cout << "\nr.x = " << r->x
+              << "\nr.y = " << r->y;
+  }
+
+  // Formatting only
+  std::cout << std::endl;
+}
+
+int main()
+{
+// /*
+  //--------------------------------------------------------------------------//
+  // Default constructor (zero initialized)
+  Widget w0{};
+  print_widget("\ndefault ctor", w0);
+
+  //--------------------------------------------------------------------------//
+  // Parameterized constructor
+  Widget w1{1, "yo", {0,1}};
+  print_widget("\n(1) parameterized ctor", w1);
+
+  //--------------------------------------------------------------------------//
+  // Copy constructor
+  Widget w2{2, "yoo", {2,3}};
+  Widget w3{w2};
+
+  print_widget("\n(2a) copy ctor (original obj)", w2);
+  print_widget("(2b) copy ctor (new obj)",      w3);
+
+  //--------------------------------------------------------------------------//
+  // Copy assignment operator
+  Widget w4{3, "foo", {4,5}};
+  Widget w5 = w4;
+
+  print_widget("\n(3a) copy assign (original obj)", w4);
+  print_widget("(3b) copy assign (new obj)",      w5);
+
+  //--------------------------------------------------------------------------//
+  // Move constructor
+  Widget w6{4, "bar", {6,7}};
+  print_widget("\n(4a) move ctor (original obj)", w6);
+
+  Widget w7{std::move(w6)};
+  print_widget("(4b) move ctor (new obj)",       w7);
+  print_widget("(4c) move ctor (post-move obj)", w6);
+
+  //--------------------------------------------------------------------------//
+  // Move assignment operator
+  Widget w8{5, "baz", {8,9}};
+  print_widget("\n(5a) move assign (original obj)", w8);
+
+  Widget w9 = std::move(w8);
+  print_widget("(5b) move assign (new obj)",       w9);
+  print_widget("(5c) move assign (post-move obj)", w8);
+// */
+}
