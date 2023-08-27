8d3bbc0fa798f1707e77818e11d0ce537523df06
Christopher Brandt <cmb199@zips.uakron.edu>
2023-08-26 22:47:14 -0400

    Update rational.cxx
    Updating example code.

--- src/1-class-essentials/rational.cxx
+++ src/1-class-essentials/rational.cxx
@@ -42,11 +42,12 @@ public:
   int get_den() const { return den; }
 
   // Mutators
-  void set_num(int n) { num = n; reduce();    }
+  void set_num(int n) { num = n; normalize(); }
   void set_den(int d) { den = d; normalize(); }
 
   // Arithmetic
   Rational& operator+=(Rational const& other);
+  Rational& operator+=(int other);
 
 private:
   void normalize();
@@ -98,8 +99,14 @@ Rational& Rational::operator+=(Rational const& other)
 {
   num = num * other.den + other.num * den;
   den = den * other.den;
-  reduce();
+  normalize();
   return *this;
+}
+
+Rational& Rational::operator+=(int other)
+{
+  Rational tmp{other};
+  return *this += tmp;
 }
 
 //
@@ -107,6 +114,18 @@ Rational& Rational::operator+=(Rational const& other)
 
 // Arithmetic
 Rational operator+(Rational const& lhs, Rational const& rhs)
+{
+  Rational tmp{lhs};
+  return tmp += rhs;
+}
+
+Rational operator+(Rational const& lhs, int rhs)
+{
+  Rational tmp{rhs};
+  return tmp += lhs;
+}
+
+Rational operator+(int lhs, Rational const& rhs)
 {
   Rational tmp{lhs};
   return tmp += rhs;
@@ -128,8 +147,8 @@ bool operator!=(Rational const& lhs, Rational const& rhs)
 bool operator<(Rational const& lhs, Rational const& rhs)
 {
   int n1 = lhs.get_num();
-  int n2 = rhs.get_num();
   int d1 = lhs.get_den();
+  int n2 = rhs.get_num();
   int d2 = rhs.get_den();
 
   return (n1 * d2) < (n2 * d1);
@@ -160,36 +179,46 @@ void print(std::string_view sv, Rational const& r)
 int main()
 {
   Rational r1;
-  print("\nDefault constructor", r1);              // = 0/1
+  print("\nDefault constructor", r1);            // = 0/1
 
   Rational r2{5};
-  print("\nInteger conversion constructor", r2);   // = 5/1
+  print("\nInteger conversion constructor", r2); // = 5/1
 
   Rational r3{2, 4};
-  print("\nParameterized constructor", r3);        // = 1/2
+  print("\nParameterized constructor", r3);      // = 1/2
 
   Rational r4{r3};
-  print("\nCopy constructor", r4);                 // = 1/2
+  print("\nCopy constructor", r4);               // = 1/2
 
   Rational r5 = r3;
-  print("\nCopy assignment operator", r5);         // = 1/2
+  print("\nCopy assignment operator", r5);       // = 1/2
 
   Rational r6{std::move(r3)};
-  print("\nMove constructor", r6);                 // = 1/2
+  print("\nMove constructor", r6);               // = 1/2
 
   Rational r7 = std::move(r4);
-  print("\nMove assignment operator", r7);         // = 1/2
+  print("\nMove assignment operator", r7);       // = 1/2
 
 
   Rational a1{-2, 7};
   Rational a2{4, -3};
   a1 += a2;
-  print("\nMember addition", a1);     // = -34/21
+  print("\nMember addition (Rational, Rational)", a1);     // = -34/21
 
   Rational a3{-2, 7};
-  Rational a4{4, -3};
-  Rational a5 = a3 + a4;
-  print("\nNon-Member addition", a5); // = -34/21
+  a3 += 1;
+  print("\nMember addition (Rational, int)", a3);          // = 5/7
+
+  Rational a4{-2, 7};
+  Rational a5{4, -3};
+  Rational a6 = a4 + a5;
+  print("\nNon-Member addition (Rational, Rational)", a6); // = -34/21
+
+  Rational a7 = a4 + 1;
+  print("\nNon-Member addition (Rational, int)", a7);      // = 5/7
+
+  Rational a8 = 1 + a4;
+  print("\nNon-Member addition (int, Rational)", a8);      // = 5/7
 
 
   Rational c1{-1, 2};
